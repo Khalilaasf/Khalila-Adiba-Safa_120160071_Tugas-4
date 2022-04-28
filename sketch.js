2f88eb5db24aa02d0f567c00db80901a86f95b55
Khalilaasf <89070744+Khalilaasf@users.noreply.github.com>
2022-04-28 09:48:37 +0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,115 @@
+let vs = []
+function setup() {
+  createCanvas(400, 400);
+  v = new Pesawat(200, 200)
+}
+
+function draw() {
+  background(220);
+  v.tampil()
+  v.tepi()
+  v.perbaharui()
+  v.terbang()
+}
+
+class Pesawat{
+  constructor(x,y){
+    this.tempat = createVector(x,y)
+    this.cepat = createVector(1,0)
+    this.percepatan = createVector(0,0)
+    this.a = 20;
+    this.maxspeed = 2;
+    this.terbangTheta = PI/2;
+  }
+  
+  terbang(){
+    let projVector = this.cepat.copy()
+    projVector.setMag(100)
+    let projPoint = projVector.add(this.tempat)
+    let terbangRadius = 50;
+    let theta = this.terbangTheta + this.cepat.heading()
+    let xBar = terbangRadius * cos(theta)
+    let yBar = terbangRadius * sin(theta)
+    let terbangPoint = p5.Vector.add(projPoint,createVector(xBar,yBar))
+    let debug = true;
+    if(debug){}
+    let driveForce = terbangPoint.sub(this.tempat);
+    driveForce.setMag(this.maxForce)
+    this.applyForce(driveForce)
+    this.terbangTheta += random(-0.5, 0.5)
+  }
+  
+  search(vectorTarget){
+    var menuju = p5.Vector.sub(vectorTraget, this.tempat)
+    menuju.normalize()
+    menuju.mult(this.maxspeed)
+    var drive = p5.Vector.sub(menuju, this.cepat)
+    drive.limit(this.maxForce)
+    this.applyForce(drive)
+  }
+  
+  tiba(vectorTarget){
+    var menuju = p5.Vector.sub(vectorTarget, this.tempat)
+    var jarak = menuju.mag()
+    if(jarak<100){
+      var b = map(jarak, 0, 100, 0, this.maxspeed)
+      menuju.normalize()
+      menuju.mult(b)
+    }
+    
+    else{
+      menuju.normalize()
+      menuju.mult(this.maxspeed)
+    }
+    
+    var drive = p5.Vector.sub(menuju, this.cepat)
+    drive.limit(this.maxForce)
+    this.applyForce(drive)
+  }
+  
+  perbaharui(){
+    this.cepat.add(this.percepatan)
+    this.cepat.limit(this.maxspeed)
+    this.tempat.add(this.cepat)
+    this.percepatan.mult(0)
+  }
+  
+  applyForce(force){
+    this.percepatan.add(force)
+  }
+  
+  tampil(){
+    var theta = this.cepat.heading()
+    push()
+    fill(71,92,108)
+    translate(this.tempat.x, this.tempat.y)
+    rotate(theta)
+    strokeWeight(0.5)
+    ellipse(35,40,28,8)
+    arc(25,38,4,10, radians(180), radians(360))
+    fill(138,133,131)
+    arc(35,36,5,15, radians(180), radians(360))
+    arc(35,42,5,20, radians(0), radians(180))
+    pop()
+  }
+  
+  tepi(){
+    if(this.tempat.x>width+10){
+      this.tempat.x = -10;
+    }
+    
+    else if(this.tempat.x<-10){
+      this.tempat.x = width+10;
+    }
+    
+    if(this.tempat.y>height+10){
+      this.tempat.y = -10;
+    }
+    
+    else if(this.tempat.y<-10){
+      this.tempat.y = height+10;
+    }
+    
+
+  }
+}
