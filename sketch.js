f96ed2ada569bb27a9d94689b399a402a2427b28
Khalilaasf <89070744+Khalilaasf@users.noreply.github.com>
2022-05-08 10:43:18 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,141 +1,133 @@
 let vs = []
 function setup() {
   createCanvas(400, 400);
-  v = new Vehicle(200,200);
+  v = new Pesawat(200, 200)
 }
 
 function draw() {
   background(220);
-  
-  v.display()
-  v.batas()
-  v.diperbarui();
-  v.terbang();
-  
+  v.tampil()
+  v.tepi()
+  v.perbaharui()
+  v.terbang()
 }
 
-class Vehicle{
+class Pesawat{
   constructor(x,y){
-    this.posisi = createVector(x,y);
-    this.kecepatan = createVector(1,0);
-    this.percepatan = createVector(0,0);
-    this.l = 30.0;
+    this.tempat = createVector(x,y)
+    this.cepat = createVector(1,0)
+    this.percepatan = createVector(0,0)
+    this.a = 30.0;
     this.maxspeed = 2;
     this.maxforce = 0.01;
     this.terbangTheta = PI/2;
   }
   
   terbang(){
-    let projVector = this.kecepatan.copy();
-    projVector.setMag(100);
-    let projPoint = projVector.add(this.posisi);
-    
+    let projVector = this.cepat.copy()
+    projVector.setMag(100)
+    let projPoint = projVector.add(this.tempat)
     let terbangRadius = 30;
-    let theta = this.terbangTheta + this.kecepatan.heading();
-    let xBar = terbangRadius * cos(theta);
-    let yBar = terbangRadius * sin(theta);
-    
-    let terbangPoint = p5.Vector.add(projPoint, createVector(xBar,yBar));
-    
+    let theta = this.terbangTheta + this.cepat.heading()
+    let xBar = terbangRadius * cos(theta)
+    let yBar = terbangRadius * sin(theta)
+    let terbangPoint = p5.Vector.add(projPoint,createVector(xBar,yBar))
     let debug = true;
     
     if (debug){
-      push()
-      fill(150, 75, 0);
-      stroke('red')
-      line(this.posisi.x, this.posisi.y, projPoint.x, projPoint.y)
-      stroke('red');
-      circle(projPoint.x, projPoint.y, 8);
-      noFill();
-      stroke('red');
-      circle(projPoint.x, projPoint.y, terbangRadius*2);
-      
-      line(this.posisi.x, this.posisi.y, terbangPoint.x, terbangPoint.y)
-      stroke('black')
-      fill('purple')
-      circle(terbangPoint.x, terbangPoint.y, 16);
-      pop()
-    }
-    
-    let mengemudiForce = terbangPoint.sub(this.posisi);
-    mengemudiForce.setMag(this.maxforce);
-    this.applyForce(mengemudiForce);
-    
-    this.terbangTheta += random(-0.5, 0.5);
-    
-  }
-  
-  mencari(vektorTarget){
-    var tujuan = p5.Vector.sub(vektorTarget, this.posisi);
-    tujuan.normalize();
-    tujuan.mult(this.maxspeed);
-    
-    //kemudi
-    var mengendarai = p5.Vector.sub(tujuan, this.kecepatan);
-    mengendarai.limit(this.maxforce);
-    this.applyForce(mengendarai);
-  }
-  
-  sampai(vektorTarget){
-    var tujuan = p5.Vector.sub(vektorTarget, this.posisi);
-    var jarak = tujuan.mag()
+    push()
+    fill(150, 75, 0);
+    stroke('red')
+    line(this.tempat.x, this.tempat.y, projPoint.x, projPoint.y)
+    stroke('red');
+    circle(projPoint.x, projPoint.y, 8);
+    noFill();
+    stroke('red');
+    circle(projPoint.x, projPoint.y, terbangRadius*2);
 
-    if (jarak < 100){
-      var m = map(jarak, 0, 100, 0, this.maxspeed);
-      tujuan.normalize();
-      tujuan.mult(m);
-      
+    line(this.tempat.x, this.tempat.y, terbangPoint.x, terbangPoint.y)
+    stroke('black')
+    fill('purple')
+    circle(terbangPoint.x, terbangPoint.y, 16);
+    pop()
+  }
+
+    let driveForce = terbangPoint.sub(this.tempat);
+    driveForce.setMag(this.maxForce)
+    this.applyForce(driveForce)
+    this.terbangTheta += random(-0.5, 0.5)
+  }
+  
+  search(vectorTarget){
+    var menuju = p5.Vector.sub(vectorTraget, this.tempat)
+    menuju.normalize()
+    menuju.mult(this.maxspeed)
+    var drive = p5.Vector.sub(menuju, this.cepat)
+    drive.limit(this.maxForce)
+    this.applyForce(drive)
+  }
+  
+  tiba(vectorTarget){
+    var menuju = p5.Vector.sub(vectorTarget, this.tempat)
+    var jarak = menuju.mag()
+    if(jarak<100){
+      var b = map(jarak, 0, 100, 0, this.maxspeed)
+      menuju.normalize()
+      menuju.mult(b)
     }
+    
     else{
-      tujuan.normalize();
-      tujuan.mult(this.maxspeed);    
+      menuju.normalize()
+      menuju.mult(this.maxspeed)
     }
-
     
-    //kemudi
-    var mengendarai = p5.Vector.sub(tujuan, this.kecepatan);
-    mengendarai.limit(this.maxforce);
-    this.applyForce(mengendari);
+    var drive = p5.Vector.sub(menuju, this.cepat)
+    drive.limit(this.maxForce)
+    this.applyForce(drive)
   }
   
-  
-  diperbarui(){
-    this.kecepatan.add(this.percepatan);
-    this.kecepatan.limit(this.maxspeed);
-    this.posisi.add(this.kecepatan);
-    this.percepatan.mult(0);
+  perbaharui(){
+    this.cepat.add(this.percepatan)
+    this.cepat.limit(this.maxspeed)
+    this.tempat.add(this.cepat)
+    this.percepatan.mult(0)
   }
+  
   applyForce(force){
-    this.percepatan.add(force);
+    this.percepatan.add(force)
   }
-  display(){
-    var theta = this.kecepatan.heading()// + PI/2;
-    push();
-    fill(175);
-    stroke(0);
-    translate(this.posisi.x, this.posisi.y)
+  
+  tampil(){
+    var theta = this.cepat.heading()
+    push()
+    fill(71,92,108)
+    translate(this.tempat.x, this.tempat.y)
     rotate(theta)
     strokeWeight(0.5)
-    fill(255, 0, 0)
     ellipse(-10, 0, 28, 8) //badan pesawat
     arc(-20, -1, 4, 10, radians(180), radians(360)) //ekor
-    fill(128)
+    fill(138,133,131)
     arc(-10,-4, 5, 15, radians(180), radians(360)) //sayap kiri
     arc(-10, 2, 5, 20, radians(0), radians(180))  //sayap kanan
-    pop();
+    pop()
   }
+  
+  tepi(){
+    if(this.tempat.x>width+10){
+      this.tempat.x = -10;
+    }
+    
+    else if(this.tempat.x<-10){
+      this.tempat.x = width+10;
+    }
+    
+    if(this.tempat.y>height+10){
+      this.tempat.y = -10;
+    }
+    
+    else if(this.tempat.y<-10){
+      this.tempat.y = height+10;
+    }
 
-   batas() {
-    if (this.posisi.x > width + 10) {
-      this.posisi.x = -10;
-    } else if (this.posisi.x < -10) {
-      this.posisi.x = width + 10;
-    }
-    if (this.posisi.y > height + 10) {
-      this.posisi.y = -10;
-    } else if (this.posisi.y < -10) {
-      this.posisi.y = height + 10;
-    }
   }
-
 }
